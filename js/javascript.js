153a9073125666c702fe1c8fff8db40ba7465d1c
f21joanne <joanne.k@forever21.com>
2017-07-11 15:13:32 -0700

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,66 +1,67 @@
 $(document).ready(function(){
+  $('.nav-links li a').on('click', function(){
+      var scrollAnchor = $(this).attr('data-scroll'),
+          scrollPoint = $('div[data-anchor="' + scrollAnchor + '"]').offset().top - 60;
 
-    $('.nav-links li a').on('click', function(){
-        var scrollAnchor = $(this).attr('data-scroll'),
-            scrollPoint = $('div[data-anchor="' + scrollAnchor + '"]').offset().top - 60;
+  $('body,html').animate({
+      scrollTop: scrollPoint
+  }, 600);
 
-    $('body,html').animate({
-        scrollTop: scrollPoint
-    }, 600);
+  return false;
+  });
 
-    return false;
-    });
+  $('.submit').on('click', function(e){
+    $('.popup').addClass('show_box');
+    $('.p_bg').addClass('show_bg');
+    e.preventDefault();
+  });
 
-    $('.submit').on('click', function(e){
-      $('.popup').attr('style', 'display:table !important');
-      e.preventDefault();
-    });
-
-    $('.popup_close').on('click', function(){
-      $('.popup').attr('style', 'display:none');
-    });
+  $('.popup_close, .p_bg').on('click', function(){
+    $('.popup').removeClass('show_box');
+    $('.p_bg').removeClass('show_bg');
+  });
 
   $(window).scroll(function(){
-      var windscroll = $(window).scrollTop();
+    var windscroll = $(window).scrollTop();
 
-      if (windscroll > 130) {
-          $('#navbar').addClass('navbar-fixed');
-          $('.section').each(function(i) {
-              if($(this).position().top - 130 <= windscroll) {
-                  $('.nav-links li a.active').removeClass('active');
-                  $('.nav-links li a').eq(i).addClass('active');
-              }
-          });
-          $('.scrollToTop').show();
-      }
-      else {
-          $('#navbar').removeClass('navbar-fixed');
-          $('.nav-links li a.active').removeClass('active');
-          $('.scrollToTop').hide();
-      }
+    if (windscroll > 130) {
+        // $('#navbar').addClass('navbar-fixed');
+        $('.section').each(function(i) {
+            if($(this).position().top - 130 <= windscroll) {
+                $('.nav-links li a.active').removeClass('active');
+                $('.nav-links li a').eq(i).addClass('active');
+            }
+        });
+        $('.scrollToTop').show();
+    }
+    else {
+        // $('#navbar').removeClass('navbar-fixed');
+        $('.nav-links li a.active').removeClass('active');
+        $('.scrollToTop').hide();
+    }
   });
 
-if (window.atob) {
-  var mySwiper = new Swiper('.swiper-container',{
-    pagination: '.pagination',
-    loop:true,
-    grabCursor: true,
-    paginationClickable: true,
-    autoplay: 3500,
-    speed: 1500
-  });
-}
+  if (window.atob) {
+    var mySwiper = new Swiper('.swiper-container',{
+      pagination: '.pagination',
+      loop:true,
+      grabCursor: true,
+      paginationClickable: true,
+      autoplay: 3500,
+      speed: 1500
+    });
+  }
 
-if (document.all && !window.atob) {
-  var mySwiper = new Swiper('.swiper-container',{
-    pagination: '.pagination',
-    loop:true,
-    grabCursor: true,
-    paginationClickable: true,
-    autoplay: 3500,
-    speed: 20000
-  });
- }
+  if (document.all && !window.atob) {
+    var mySwiper = new Swiper('.swiper-container',{
+      pagination: '.pagination',
+      loop:true,
+      grabCursor: true,
+      paginationClickable: true,
+      autoplay: 3500,
+      speed: 20000
+    });
+   }
 
   $('.arrow-left').on('click', function(e){
     e.preventDefault()
@@ -71,19 +72,19 @@ if (document.all && !window.atob) {
     mySwiper.swipeNext()
   });
 
-    $('.toggle-nav').click(function(){
-        $('.nav').addClass('show_div');
-        $("body").css("overflow","hidden");
-    });
+  $('.toggle-nav').click(function(){
+      $('.nav').addClass('show_div');
+      $("body").css("overflow","hidden");
+  });
 
-    $('.close, .carousel, .container, footer, .nav-links li a').click(function(){
-        $('.nav').removeClass('show_div');
-        $("body").css("overflow","visible");
-    });
+  $('.close, .carousel, .container, footer, .nav-links li a').click(function(){
+      $('.nav').removeClass('show_div');
+      $("body").css("overflow","visible");
+  });
 
-    $('.scrollToTop').click(function(){
-        $('html, body').animate({
-            scrollTop: 0
-        }, 600);
-    });
+  $('.scrollToTop').click(function(){
+      $('html, body').animate({
+          scrollTop: 0
+      }, 600);
+  });
 });
